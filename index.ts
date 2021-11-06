3db2e5526cbff237d59fb91c6269c7f7a805be85
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-06 01:10:22 -0600

    Create app with express and TypeScript

---
+++ index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import routes from "./routes";
+
+//Middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+//Database
+
+//Routes
+app.get("/api/v1", (req, res) => {
+  res.json({ msg: "Bienvenido a la REST API de Peña Colorada" });
+});
+app.use("/api/v1/grata", routes.grataRouter);
+app.use("/api/v1/auth", routes.authRouter);
+app.use("/api/v1/solicitudes", routes.solicitudesRouter);
+app.use("/api/v1/red-ascenso", routes.redAscensoRouter);
+
+//Server listening
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
