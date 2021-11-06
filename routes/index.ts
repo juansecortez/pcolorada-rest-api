7fcf1b95c8cf10f7d0aedebc9041982cf7cf8186
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-06 01:17:10 -0600

    Added index router

---
+++ routes/index.ts
@@ -0,0 +1,13 @@
+import grataRouter from "./grataRouter";
+import authRouter from "./authRouter";
+import solicitudesRouter from "./solicitudesRouter";
+import redAscensoRouter from "./redAscensoRouter";
+
+const routes = {
+  grataRouter,
+  authRouter,
+  solicitudesRouter,
+  redAscensoRouter,
+};
+
+export default routes;
