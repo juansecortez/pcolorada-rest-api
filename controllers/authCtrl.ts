681795b77db2b4d9cb5d0bb4269f10f88d80a9d7
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-12-11 13:27:19 -0600

    Update login eliminate active directory

--- controllers/authCtrl.ts
+++ controllers/authCtrl.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import ActiveDirectory from "activedirectory2";
 import axios from "axios";
 import {
   generateAccessToken,
@@ -7,82 +6,59 @@ import {
 } from "../config/generateToken";
 import jwt from "jsonwebtoken";
 import { IDecodeToken, IUserData } from "../interfaces";
-export const configActiveDirectory = {
-  url: `${process.env.AD_DOMAIN}`,
-  username: `${process.env.AD_USER}`,
-  password: `${process.env.AD_PASSWORD}`,
-  baseDN: ``,
-};
+import { getconectionGratas } from "../config/database";
 
 const authCtrl = {
-  loging: async (req: Request, res: Response) => {
-    var ad = new ActiveDirectory(configActiveDirectory);
-    console.log(configActiveDirectory, ad);
-    const { username, password } = req.body;
-    ad.authenticate(username, password, (err: any, auth) => {
-      if (err) {
-        console.log(err);
-        if (err.code === 49) {
-          return res.status(500).json({ msg: "Servidor no disponible" });
-        }
-        let error: any = JSON.stringify(err);
-        if (error.includes("ENOTFOUND")) {
-          return res.status(500).json({ msg: "Servidor no disponible" });
-        }
-        if (error.includes("ETIMEDOUT")) {
-          return res.status(500).json({ msg: "Servidor no disponible" });
-        }
-      }
-      try {
-        if (auth) {
-          const access_token = generateAccessToken({ username });
-          const refresh_token = generateRefreshToken({ username });
-          res.cookie("refreshtoken", refresh_token, {
-            httpOnly: true,
-            path: "/api/refresh_token",
-            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-          });
-          res.json({
-            msg: "Login Success!",
-            access_token,
-          });
-        } else {
-          console.log(err);
-          console.log(auth);
-          res
-            .status(404)
-            .json({ msg: "El usuario o la contraseña son incorrectos" });
-        }
-      } catch (error) {
-        return res
-          .status(500)
-          .json({ msg: "Ocurrio un error contacte con el administrador" });
-      }
-    });
-  },
   logout: async (req: Request, res: Response) => {
     try {
       res.clearCookie("refreshtoken", {
-        path: "/api/refresh_token",
+        path: "/api/v1/auth/refresh_token",
       });
-      return res.json({ msg: "Logged out!" });
+      return res.json({ message: "Logged out!" });
     } catch (error) {
-      return res.status(500).json({ msg: error });
+      return res.status(500).json({ message: "No hay servicio" });
     }
   },
   refreshToken: async (req: Request, res: Response) => {
     try {
       const rf_token = req.cookies.refreshtoken;
-      if (!rf_token) return res.status(400).json({ msg: "Please login now!" });
+      if (!rf_token)
+        return res.status(401).json({ message: "Please login now!" });
+      console.log("hire");
       const decoded = <IDecodeToken>(
         jwt.verify(rf_token, `${process.env.REFRESH_TOKEN_SECRET}`)
       );
-      if (!decoded.username)
-        return res.status(400).json({ msg: "Please login now!" });
-      const access_token = generateAccessToken({ username: decoded.username });
-      res.json({ access_token, username: decoded.username });
+      if (!decoded.USUARIOID)
+        return res.status(401).json({ message: "Please login now!" });
+
+      const params = new URLSearchParams();
+      params.append("usuarioid", decoded.USUARIOID);
+      const result = await axios.post(
+        "http://vwebdelta/WebDataSap/Service1.asmx/EXISTE_USUARIO",
+        params
+      );
+      if (typeof result.data === "string") {
+        return res.json({ message: "No existe el usuario" });
+      }
+      const user: IUserData = result.data[0];
+      if (!user)
+        return res
+          .status(400)
+          .json({ message: "This account does not exist." });
+      const { USUARIOID, NOMBRE, NOEMPLEADO, direcciones } = decoded;
+      const access_token = generateAccessToken({
+        USUARIOID,
+        NOMBRE,
+        NOEMPLEADO,
+        direcciones,
+      });
+      res.json({
+        access_token,
+        user: { USUARIOID, NOMBRE, NOEMPLEADO },
+        direcciones,
+      });
     } catch (error: any) {
-      return res.status(500).json({ msg: error.message });
+      return res.status(500).json({ message: "No hay servicio" });
     }
   },
   loginSOAP: async (req: Request, res: Response) => {
@@ -99,17 +75,44 @@ const authCtrl = {
         return res.json({ message: "El usuario o la contraseña estan mal" });
       }
       const user: IUserData = result.data[0];
-      const access_token = generateAccessToken({ id: user.USUARIOID });
-      const refresh_token = generateRefreshToken({ id: user.USUARIOID });
+      const { USUARIOID, NOMBRE, NOEMPLEADO } = user;
+      const pool1 = await getconectionGratas();
+      if (pool1 === false) {
+        return;
+      }
+      const resultado = await pool1.query(`USE GRATA
+      SELECT id_direccion as IdDireccion FROM usuarios WHERE usuario_id = '${USUARIOID}'`);
+      var direcciones = [];
+      for (var value in resultado.recordsets[0]) {
+        direcciones.push(resultado.recordsets[0][value].IdDireccion);
+      }
+      pool1.close();
+      const access_token = generateAccessToken({
+        USUARIOID,
+        NOMBRE,
+        NOEMPLEADO,
+        direcciones,
+      });
+      const refresh_token = generateRefreshToken({
+        USUARIOID,
+        NOMBRE,
+        NOEMPLEADO,
+        direcciones,
+      });
       res.cookie("refreshtoken", refresh_token, {
         httpOnly: true,
-        path: "/api/refresh_token",
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        path: "/api/v1/auth/refresh_token",
+        maxAge: 172_800_000, //  Dos dias
       });
       res.json({
-        msg: "Login Success!",
+        message: "Login Success!",
         access_token,
-        user,
+        user: {
+          USUARIOID,
+          NOMBRE,
+          NOEMPLEADO,
+        },
+        direcciones,
       });
     } catch (error: any) {
       console.log(error.message);
