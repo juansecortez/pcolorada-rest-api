5c54b4b021a8eb42cab79817f8a10a914646de2e
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-12-11 13:28:56 -0600

    Added interfaces

---
+++ interfaces/index.ts
@@ -0,0 +1,32 @@
+import { Request } from "express";
+
+export interface IDecodeToken {
+  USUARIOID: string;
+  NOMBRE: string;
+  NOEMPLEADO: string;
+  direcciones: [];
+  iat: number;
+  exp: number;
+}
+export interface IGrata {
+  presupuestoFinanzas: number;
+  presupuestoMinas: number;
+  presupuestoPelet: number;
+  presupuestoRH: number;
+  presupuestoGeneral: number;
+  presupuestoBeneficio: number;
+  presupuestoTec: number;
+  presupuestoDirectores: number;
+  anio: number;
+  fechaFin: string;
+  fechaInicio: string;
+}
+export interface IUserData {
+  USUARIOID: string;
+  NOMBRE: string;
+  NOEMPLEADO: string;
+}
+export interface IReqAuth extends Request {
+  user?: IUserData;
+  direcciones?:[]
+}
