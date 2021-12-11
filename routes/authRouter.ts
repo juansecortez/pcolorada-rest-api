3d68c0f27f9e99c8271e633589ccd1461aa8aae2
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-12-11 13:28:19 -0600

    Added endpoints refresh token and logout

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import authCtrl from "../controllers/authCtrl";
 const router = Router();
 
-router.post("/login", authCtrl.loging);
+router.post("/login", authCtrl.loginSOAP);
+router.get("/refresh_token", authCtrl.refreshToken);
+router.get("/logout", authCtrl.logout);
 
 export default router;
