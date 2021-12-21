2b72610635d59981558aa3f5696e3f41d25e7c53
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-12-21 12:31:26 -0600

    Added errors by create grata

--- utils/createGrata.ts
+++ utils/createGrata.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from "express";
 import { getconectionGratas, getconectionVDBDELTA } from "../config/database";
 
 const newGrata = async (
@@ -8,39 +9,36 @@ const newGrata = async (
   presupuesto: number
 ) => {
   const pool1 = await getconectionVDBDELTA();
-
   //Ejecutar el procedimiento almacenado de SQL SERVER
   if (pool1 === false) {
-    return;
+    throw new Error("No hay conexion");
   }
-  try {
-    const respond = await pool1
-      .request()
-      .query(`EXEC [dbo].[DATOS_GRATA] '${direccion}','${anio}'`);
-    const { recordsets } = respond;
-    if (Object.keys(recordsets[0]).length === 0) {
-      return;
-    }
+  const respond = await pool1
+    .request()
+    .query(`EXEC [dbo].[DATOS_GRATA] '${direccion}','${anio}'`);
+  const { recordsets } = respond;
+  if (Object.keys(recordsets[0]).length === 0) {
     pool1.close();
-    const pool = await getconectionGratas();
-    if (pool === false) {
-      return;
-    }
-    const data = JSON.stringify(recordsets[0]);
-    const respondG = await pool.request().query(`USE GRATA
-        EXEC [dbo].[createNewGrata] '${data}','${direccion}','${anio}','${fecha_Inicio}','${fecha_Fin}','${presupuesto}'`);
-    const { recordsets: respond2 } = respondG;
-    if (Object.keys(respond2).length === 0) {
-      pool.close();
-      return;
-    } else {
-      pool.close();
-      return true;
-    }
-  } catch (error) {
-    console.log(error);
-    return;
+    throw new Error("Esa grata no existe");
   }
+  pool1.close();
+  const pool = await getconectionGratas();
+  if (pool === false) {
+    throw new Error("No hay conexion");
+  }
+  const data = JSON.stringify(recordsets[0]);
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  const dateTime_Creacion = today.toISOString();
+  const respondG = await pool.request().query(`USE GRATA
+        EXEC [dbo].[createNewGrata] '${data}','${direccion}','${anio}','${fecha_Inicio}','${fecha_Fin}','${presupuesto}','${dateTime_Creacion}'`);
+  const { recordsets: respond2 } = respondG;
+  if (Object.keys(respond2).length === 0) {
+    pool.close();
+    throw new Error("No creo la grata");
+  }
+  pool.close();
+  pool1.close();
 };
 
 export default newGrata;
