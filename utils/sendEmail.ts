a4bdc66f3ccd69193a6256d152bb7a0e5f82c973
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-12-21 12:32:09 -0600

    Modify format email

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,6 +1,11 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async (to: string, url: string, txt: string) => {
+export const sendEmail = async (
+  to: string,
+  url: string,
+  txt: string,
+  anio: number
+) => {
   try {
     const SENDER_EMAIL = `${process.env.SENDER_EMAIL_ADDRESS}`;
     const SMTP_HOST = `${process.env.SMTP_HOST}`;
@@ -15,19 +20,22 @@ export const sendEmail = async (to: string, url: string, txt: string) => {
       from: SENDER_EMAIL,
       to: to,
       subject: txt,
-      html: ` <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-    <h2 style="text-align: center; text-transform: uppercase;color: teal;">Grata iniciada</h2>
-    <p>
-    Prueba grata
-    </p>
-  
-  <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
-  
-  <p>If the button doesn't work for any reason, you can also click on the link below:</p>
-  
-  <div>${url}</div>
-  </div>
-  `,
+      attachments: [
+        {
+          filename: "start.png",
+          path: "assets/start.png",
+          cid: "start", //my mistake was putting "cid:logo@cid" here!
+        },
+      ],
+      html: `
+      <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+      <h2 style="text-align: center; text-transform: uppercase;color: teal;">Inicio de grata ${anio}</h2>
+      <p style="text-align:center">${txt}</p>
+      <div style="display: flex; justify-content: center; align-items: center;">
+      <a href="${url}"><img  width="150" src="cid:start"></a>
+      </div>
+      </div>
+      `,
     };
     const result = await transport.sendMail(mailOptions);
     return result;
