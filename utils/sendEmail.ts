86ace194a16a408f813226a2c5b032e077bf5c80
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2022-01-31 23:52:45 -0600

    Update util send email

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import nodemailer from "nodemailer";
 /**
  * Función para envio de correos hacia un servidor de SMTP
@@ -6,7 +7,7 @@ export const sendEmail = async (
   to: string,
   url: string,
   txt: string,
-  anio: number
+  title: string
 ) => {
   const SENDER_EMAIL = `${process.env.SENDER_EMAIL_ADDRESS}`;
   const SMTP_HOST = `${process.env.SMTP_HOST}`;
@@ -30,14 +31,14 @@ export const sendEmail = async (
     ],
     html: `
       <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-      <h2 style="text-align: center; text-transform: uppercase;color: teal;">Inicio de grata ${anio}</h2>
+      <h2 style="text-align: center; text-transform: uppercase;color: teal;">${title}</h2>
       <p style="text-align:center">${txt}</p>
       <div style="display: flex; justify-content: center; align-items: center;">
-      <a href="${url}"><img  width="150" src="cid:start"></a>
+      <a href="${url}">START</a>
       </div>
       </div>
       `,
   };
-  const result = await transport.sendMail(mailOptions);
-  return result;
+
+  await transport.sendMail(mailOptions);
 };
