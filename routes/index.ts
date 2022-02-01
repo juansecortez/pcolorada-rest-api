218524db2d0aeef269be3fa1a1e325c110cef476
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2022-01-31 23:52:12 -0600

    Added enpoint

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,13 @@
 import grataRouter from "./grataRouter";
 import authRouter from "./authRouter";
 import workersGrataRouter from "./workersGrataRouter";
+import periodRouter from "./periodRouter";
 
 const routes = {
   grataRouter,
   authRouter,
   workersGrataRouter,
+  periodRouter
 };
 
 export default routes;
