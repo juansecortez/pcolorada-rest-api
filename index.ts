f55d1340bc4bdbd9358561523659cd758326e508
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2022-02-02 14:13:06 -0600

    Cors

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: `${process.env.URL_CLIENT}`,
+    origin: "*",
     credentials: true,
   })
 );
